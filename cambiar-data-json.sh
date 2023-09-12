66f84cbe0f393951bc1c7b16c3d5b81c0dfc7f8e
NEBULA1978 <rpascualey@gmail.com>
2023-09-12 12:49:23 +0200

    Creo cambiar-data-json.sh para modificar archivo data.json con resultdo script anterior cambiar_nombres.sh

---
+++ cambiar-data-json.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ruta al archivo data_json.txt
+archivo_data_json="data_json.txt"
+
+# Ruta al archivo data.json
+archivo_json="data.json"
+
+# Verificar si el archivo data_json.txt existe
+if [ ! -f "$archivo_data_json" ]; then
+    echo "El archivo $archivo_data_json no existe."
+    exit 1
+fi
+
+# Copiar el contenido de data_json.txt a data.json
+cp "$archivo_data_json" "$archivo_json"
+
+echo "Contenido de $archivo_data_json copiado y actualizado en $archivo_json."
